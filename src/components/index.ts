cb4af105208945ea5930f12b6dfb6fc4c3a9b357
Weber Dantas <wdantas89@gmail.com>
2022-10-14 22:45:12 -0300

    Finalização da tela principal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import { Text } from './Text/Text';
 import { Heading } from './Heading/Heading';
 import { Button } from './Button/Button';
+import { Checkbox } from './Checkbox/Checkbox';
+import { TextInput } from './TextInput/TextInput';
 
-export { Text, Heading, Button };
+export { Text, Heading, Button, Checkbox, TextInput };
